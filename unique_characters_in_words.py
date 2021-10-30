69e71046a96d060e799328016ec42c98816ce96f
mango-days <77330707+mango-days@users.noreply.github.com>
2021-10-30 18:54:57 +0530

    Update unique_characters_in_words.py

--- unique_characters_in_words.py
+++ unique_characters_in_words.py
@@ -1,7 +1,8 @@
-str="abc"
-reverse=""
-index=0+1
-while index<=len(str):
-    reverse+=str[-index]
-    index+=1
-print(reverse)
+array=["go","bat","me","eat","goal","boy","run"]
+str1="".join(array)
+dict={}
+for x in str1:
+    if x in dict.keys(): dict[x]+=1
+    else: dict[x]=1
+for x,y in dict.items():
+    if y==1: print(x)
